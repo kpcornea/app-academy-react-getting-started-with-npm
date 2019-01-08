ed0f2b352bc9e559005bcbd86b3e2df2b54c8bb9
Kevin Cornea <kpcornea@gmail.com>
2019-01-07 22:59:45 -0800

    Add correct webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,54 @@
+// module.exports = {
+//   entry: "./entry.jsx",
+//   output: {
+//     path: "./",
+//     filename: "bundle.js",
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /(node_modules)/,
+//         loader: 'babel',
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: ["", ".js", ".jsx" ]
+//   }
+// };
+
+// ask question about this i think.
+
+const path = require('path');
+
 module.exports = {
-  entry: "./entry.jsx",
+  context: __dirname,
+  entry: "./index.jsx",
   output: {
-    path: "./",
-    filename: "bundle.js",
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
+        test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
       }
     ]
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ["", ".js", ".jsx" ]
+    extensions: [".js", ".jsx", "*"]
   }
 };
